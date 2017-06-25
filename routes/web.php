d24a8f086a839655ef9f49d2f883c9cf156900ca
belhard-user <ldarik2l@gmail.com>
2017-06-25 17:49:30 +0300

    Add routes

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,30 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
+});
+Route::get('pets', function() {
+    return 'Страница с питомцами';
+});
+Route::get('pets/{name}', function ($name){
+    return "Страница с $name";
+});
+Route::get('about', function (){
+    return "Страница о нас";
+});
+Route::get('become-volunteer', function (){
+   return "Страница волонтёров";
+});
+Route::get('contacts', function (){
+    return "Страница с контактами";
+});
+Route::get('help', function (){
+    return "Страница помощи";
+});
+Route::get('news', function (){
+    return "Страница новостей";
+});
+Route::get('news/{slag}', function ($slag){
+    return "Страница c новостью - $slag";
 });
