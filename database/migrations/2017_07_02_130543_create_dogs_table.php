2d165258e11374ebe6582f6468da5ddad1717eb8
Helene <lenagritsuk@inbox.ru>
2017-07-02 17:19:27 +0300

    Make DB

---
+++ database/migrations/2017_07_02_130543_create_dogs_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('dogs', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 40);
+            $table->tinyInteger('gender')->unsigned();
+            $table->string('color');
+            $table->tinyInteger('age')->unsigned();
+            $table->integer('height');
+            $table->string('wool');
+            $table->text('character');
+            $table->text('vet');
+            $table->text('information');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('dogs');
+    }
+}
